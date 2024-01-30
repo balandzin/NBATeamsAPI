db4b7ec8be3cc22b6b5ff604b701730cb8d61653
balandzin <balandzin2602@gmail.com>
2024-01-30 10:41:01 +0300

    Initial Commit

---
+++ NBATeamsAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBATeamsAPI
+//
+//  Created by Антон Баландин on 30.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
