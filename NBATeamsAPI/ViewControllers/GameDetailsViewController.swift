9c0faf11af2caf591db136b985c9c2803424c3e3
balandzin <balandzin2602@gmail.com>
2024-02-03 18:47:48 +0300

    Рефакторинг кода

--- NBATeamsAPI/ViewControllers/GameDetailsViewController.swift
+++ NBATeamsAPI/ViewControllers/GameDetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GameDetailsViewController: UIViewController {
+final class GameDetailsViewController: UIViewController {
     
     @IBOutlet var gameDetailsLabel: UILabel!
     
